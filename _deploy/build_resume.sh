2ad5d01cd5dc52b4ab410fad4584ce9bb9168bf1
Eli W. Hunter <hunter.eli.w@gmail.com>
2019-07-26 00:19:39 +0000

    Pin md2resume version
    Fixes #4

--- _deploy/build_resume.sh
+++ _deploy/build_resume.sh
@@ -3,8 +3,8 @@
 # Create resume and then run Jekyll site
 # This uses https://github.com/there4/markdown-resume
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/.."
 
 # Make resume PDF
-alias md2resume="docker run -v ${PWD}:/resume there4/markdown-resume md2resume"
+alias md2resume="docker run -v ${PWD}:/resume there4/markdown-resume:2.3.1 md2resume"
 md2resume pdf --template modern ./assets/resume.md .
